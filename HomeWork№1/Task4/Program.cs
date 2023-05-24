dd2a4fe8f2af1b7d39b7f22f979f886e52dad3b1
Darth_Stalin <cusima123@mail.ru>
2023-05-24 22:35:01 +0900

    Сделали задачу №4

---
+++ HomeWork№1/Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int x = 1;
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+while (x <= number)
+{
+    if (x % 2 == 0)
+    {
+        Console.WriteLine(x);
+    }
+    x++;
+}
